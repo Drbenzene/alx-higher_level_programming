757e29e66cde8d88e7c6e3ee04f027167a0c7b81
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-07 19:26:30 +0000

    Correcting completed tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,13 +1,8 @@
 #include "lists.h"
-
 /**
-
  * check_cycle - checks if a singly lined list has a cycle
-
  * @list: the list we're referring to
-
  * Return: 0 if there is no cycle, 1 if there is a cycle
-
  */
 
 int check_cycle(listint_t *list)
