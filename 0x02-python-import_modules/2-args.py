3af1ed1f3465b405763fa02c652cf36be5d32aed
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-09 17:20:09 +0000

    Adding solution

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ def main(argv):
         print("0 arguments")
     elif len(argv) - 1 == 1:
         print("{:d} arguments:".format(len(argv) - 1)
-              print("{:d} arguments: {}".format(len(arv) - 1, argv[])
+        print("{:d}: {}".format(len(argv) - 1, argv[1]))
     else:
         print("{:d}: {}".format(len(argv) - 1, argv[1]))
         for i, x in enumerate(argv[1:], 1):
-                    print("{:d}: {}".format(i, x))
+              print("{:d}: {}".format(i, x))
 if __name__ == "__main__":
-    main()
+    import sys
