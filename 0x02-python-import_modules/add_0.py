b37fe53276036efaaf74268a6a8ccff29a954093
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-09 12:06:04 +0000

    adding task 0

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a,b):
+    """Addition Function
+    Args:
+    a = First Integer
+    b = Second Integer
+    Result:
+    The result = a + b
+    """
+    return(a + b)
