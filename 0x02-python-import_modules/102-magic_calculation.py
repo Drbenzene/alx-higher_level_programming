b12694063ba8f1034a54e598f53268c5e7ad3828
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-10 06:11:39 +0000

    correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,11 +4,12 @@ def magic_calculation(a, b):
 
     if a < b:
         c = add(a , b)
-        for i in range(4. 6):
+        for i in range(4, 6):
             c = add(c, i)
             return(c)
     else:
         return sub(a,b)
 
 if __name__ == "__main__":
-    main()
+    import dis
+    dis.dis(magic_calculation)
