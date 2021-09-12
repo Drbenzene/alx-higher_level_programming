5ece3e753427a088cbab8d7cd9144282cfe9392b
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-12 17:37:32 +0000

    Pushing completed tasks

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list:
+        my_list.sorted()
+        my_list.reversed()
+        return(my_list[0])
+
+    else:
+        return(None)
