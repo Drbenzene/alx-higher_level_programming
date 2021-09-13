50637b5cdd2232ba3e69c43d97f95e2f3b69c8e7
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-13 14:58:37 +0000

    Correction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
-    for i in range(1,101):
-        if i % 3 ==0 and i % 5 == 0:
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBizz", end=' ')
         elif i % 3 == 0:
             print("Fizz", end=' ')
