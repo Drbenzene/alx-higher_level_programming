d515ee464f137f24af97f1f03c3a7905c7162483
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-14 10:07:11 +0000

    Adding solution

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import variable_load_5 as v
-def main():
-    print("{:d}".format(v.a))
-if __name__ ==  "__main__":
-    main()
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{:d}".format(a))
