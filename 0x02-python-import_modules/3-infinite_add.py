e73e9581a37d5266d6ab96af93d00e395f876ccf
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-14 09:34:23 +0000

    Updating task 4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-def main(argv):
-
+"""
+DocString Goes Here
+"""
+if __name__ == "__main__":
+    from sys import argv
     result = 0
 
-    for i, x in enumerate(argv[1:], 1):
+    for i, x in enumerate(argv[1:]):
         result += int(x)
 
         print("{:d}".format(result))
-
-if __name__ == "__main__":
-    import sys
-    main(sys.argv)
