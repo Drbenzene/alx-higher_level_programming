e713a1f127c593b55bc4596b54dbb4fd46bcc632
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-14 15:49:56 +0000

    Bugs

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
-
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i in my_list:
+    for i in range(len(my_list)):
         if i % 2 is 0:
             new_list.append(True)
         else:
