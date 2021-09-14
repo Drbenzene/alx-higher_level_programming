b118bb3e49bb4f3e7c9f3a3aaa6ec83f3ebc26e6
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-14 12:11:21 +0000

    Bug fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-import hidden_4
-
-def main():
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print(i)
-
 if __name__ == "__main__":
-    main()
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
