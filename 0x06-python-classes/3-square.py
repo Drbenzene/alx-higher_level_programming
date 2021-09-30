8d8fac076768a83cdfe7f21c5099fadd655dd08a
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-09-30 14:46:31 +0000

    Updating solution

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python
-""" Defining Class Square"""
+#!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initialize a new Square.
 
         Args:
         size (int): The size of the new square.
+
         """
 
         if not isinstance(size, int):
@@ -19,5 +20,5 @@ class Square:
         self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
+        """Return the area of the square"""
         return(self.__size * self.__size)
