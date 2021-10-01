3734e87c530ce1f8616c1c7e14c22f9349e75b8f
Boyinbode Ebenezer Ayomide0 <enegxi1@gmail.com>
2021-10-01 14:35:46 -0500

    Adding Matrix

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Adding Two Integers"""
+
+def add_integer(a, b=98):
+
+    """
+    Add Two Integers
+
+    Args:
+    a: is a integer
+    b: is a integer
+    Return is the sum of (a + b)
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!usr/bin/pyton3
+"""Divide all elements of a matrix"""
+
+error = "matrix must be a matrix (list of lists) of integers/floats"
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements if matrix"""
+    new_list = []
+
+    validate_matrix(matrix)
+    validate_div(div)
+
+    for idx in matrix:
+        new_list.append(list(map(lambda x: round(x div, 2), idx)))
+
+    return new_list
+
+
+def validate_matrix(matrix):
+    """Validating test matrix"""
+    if type(matrix) != list or len(matrix) == 0 matrix[0] is None:
+        raise TypeError(error)
+
+    for idx in matrix:
+        if len(idx) == 0:
+            raise TypeError(error)
+        elif len(matrix[0]) != len(idx):
+            raise TypeError("Each row of the matrix must have the same size")
+        else:
+            for jdx in idx:
+                if not isinstance(jdx, (int, float)):
+                    raise(error)
+
+
+            
+def validate_div(div):
+    """Validate cases div"""
+    if not isinstance(div, (int, float)):
+        raise TypeError("div muast be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
