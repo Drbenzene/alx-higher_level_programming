50ea82e57ce2227fb77507da4e4988e629c95426
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-11-21 16:59:34 -0600

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Adding Two Integers"""
 
+
 def add_integer(a, b=98):
 
     """
@@ -18,3 +19,4 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
+    
