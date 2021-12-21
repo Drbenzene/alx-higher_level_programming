d7057eb53cbffe2ca45a360f39f0323bef2bf178
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-12-21 07:39:34 -0600

    lang fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-    console.log(line);
-  }
+const lang = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (i = 0; i < lang.length; i++){
+  console.log(lang[i]);
+}
