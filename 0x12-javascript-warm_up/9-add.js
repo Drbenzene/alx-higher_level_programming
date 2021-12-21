ebd4adc7340cbeec7a6ec0a5803569c99f1c987a
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-12-21 08:05:38 -0600

    fx

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@ const a = Math.floor(Number(process.argv[2]));
 const b = Math.floor(Number(process.argv[3]));
 console.log(add(a, b));
 
-function add(a, b) {
+function add (a, b) {
     return a + b;
 }
