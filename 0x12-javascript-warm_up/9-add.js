f65707a75eef269d61af34c4361ef3eee94d5d87
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-12-21 07:52:47 -0600

    Fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const a = Math.floor(Number(process.argv[2]));
+const b = Math.floor(Number(process.argv[3]));
+console.log(add(a, b));
+
+function add(a, b) {
+    return a + b;
+}
